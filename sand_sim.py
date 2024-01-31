9a87c7421d51fa4aec22c40e059c36ce8cae7567
zZekken2 <94545042+zZekken2@users.noreply.github.com>
2024-01-31 06:44:01 +0000

    Numpy and Threading
    By adding Numpy to handle the matrices it already made a notable difference in performance
    So with threading doing the shuffling it was incomparable to the previous version

--- sand_sim.py
+++ sand_sim.py
@@ -1,7 +1,9 @@
 import pygame as pg
+from pygame.locals import *
 import time
 import random
-from pygame.locals import *
+import numpy as np
+import threading
 
 pg.init()
 
@@ -17,11 +19,10 @@ black = (0, 0, 0)
 white = (255, 255, 255)
 
 # Grid
-cell_size = 10
+cell_size = 5
 cols = screenW // cell_size
 rows = screenH // cell_size
-grid = [[0 for _ in range(cols)] for _ in range(rows)]
-prev_grid = [[0 for _ in range(cols)] for _ in range(rows)]
+grid = np.zeros((cols, rows), dtype=int)
 
 
 
@@ -39,37 +40,49 @@ def rate_limit(interval):
     return decorator
 
 
+def cellMovement(col, row, new_col):
+    grid[col, row] = 0
+    grid[new_col, row+1] = 1
+
+
+column_order = list(range(cols))
+def randomShuffle():
+    random.shuffle(column_order)
+
+
+def randomChoice(values):
+    return random.choice(values)
+
+
 def moveSand():
     # this method makes the columns be processed in a random order making the falling movement more symmetrical
-    column_order = list(range(cols))
-    random.shuffle(column_order)
+    col_thread = threading.Thread(target=randomShuffle, args=())
+    col_thread.start()
+    col_thread.join()
     for i in column_order: # the previous method (for i in range(cols)) made the sand, when falling to the right, "spawn" in place and gave the effect of rising up
         for j in range(rows-1, -1, -1): # if the end value is 0 the topmost row is not "considered" for the sand to spawn
             if j+1 <= rows-1:
-                below = grid[i][j+1];
-                if grid[i][j] == 1:
+                below = grid[i, j+1];
+                if grid[i, j] == 1:
                     if below == 0:
-                        grid[i][j] = 0
-                        grid[i][j+1] = 1
+                        cellMovement(i, j, i)
                     else:
                         # To make the sand pile up more naturally, a little bit of randomness helps :)
-                        rand = random.choice([-1, 1])
+                        rand = randomChoice([-1, 1])
 
-                        belowA = grid[i+rand][j+1] if i + rand in range(cols) else 1
-                        belowB = grid[i-rand][j+1] if i-rand in range(cols) else 1
+                        belowA = grid[i+rand, j+1] if i+rand in range(cols) else 1
+                        belowB = grid[i-rand, j+1] if i-rand in range(cols) else 1
                         if belowA == 0:
-                            grid[i][j] = 0
-                            grid[i+rand][j+1] = 1
+                            cellMovement(i, j, i+rand)
                         elif belowB == 0:
-                            grid[i][j] = 0
-                            grid[i-rand][j+1] = 1
+                            cellMovement(i, j, i-rand)
 
 
 def drawSand():
     moveSand()
     for i in range(cols):
         for j in range(rows):
-            if grid[i][j] > 0:
+            if grid[i, j] > 0:
                 pg.draw.rect(screen, white, ((i * cell_size, j * cell_size), (cell_size, cell_size)))
 
 
@@ -77,19 +90,18 @@ def drawSand():
 # Creating Sand by pressing the mouse
 def mouseSand():
     # Sand spawning area
-    radius = random.randint(5, 8)
-    sand_radius = radius // 2
+    sand_radius = cell_size // 2
     mouse_state = pg.mouse.get_pressed()
     if mouse_state[0]:
         mouseX, mouseY = pg.mouse.get_pos()
         for i in range(-sand_radius, sand_radius, 1):
             for j in range(-sand_radius, sand_radius, 1):
-                if random.choice([True, False]):
+                if randomChoice([True, False]):
                     col = (mouseX // cell_size) + i
                     row = (mouseY // cell_size) + j
                     if col in range(cols) and row in range(rows): # checks if the mouse is inside the window 
-                        if grid[col][row] == 0: # prevents summoning sand inside cells that already has it
-                            grid[col][row] = 1
+                        if grid[col, row] == 0: # prevents summoning sand inside cells that already has it
+                            grid[col, row] = 1
 
 
 # Main loop
@@ -103,4 +115,4 @@ while running:
     mouseSand()
     drawSand()
     pg.display.flip()
-    clock.tick(45)
+    clock.tick(60)
